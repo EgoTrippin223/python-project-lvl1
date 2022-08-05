55bcf2481f4bd8bcdac01c2c1190d2a3a0134444
Даниил Филимошин <filimoshin03@inbox.ru>
2022-08-06 07:19:08 +1000

    add new file and new content

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-from cli import welcome_user
+from brain_games.cli import welcome_user
+
 
 def greet():
     print('Welcome to the Brain Games!')
 
+
 def main():
     greet()
 
+
 if __name__ == '__main__':
     greet()
-if __name__ == '__main__':
     welcome_user()
