67b4eafc9bf854ebc7504883e12f5a264862da3e
Даниил Филимошин <filimoshin03@inbox.ru>
2022-08-17 19:26:49 +1000

    add new files

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -1,25 +1,34 @@
 import prompt
-from brain_games.scripts.brain_games import main
 import random
-from operator import add, sub, mul
+import operator
 
-#Математические выражения
-add, sub, mul = '+', '-', '*'
-op = random.choice(add, sub, mul)
-
-beg1 = random.randint(1, 100)
-beg2 = random.randint(1, 100)
 
 def expression():
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string("May I have your name? ")
     print("What is the result of the expression?")
     index = 0
     winscore = 3
-    op, function = random.choice(op)
+    congrats = f"Congratulations, {user_name}"
     while index < winscore:
+        operator = ["-", "+", "*"]
+        op = random.choice(operator)
+        num1 = random.randint(10, 30)
+        num2 = random.randint(1, 10)
+        answer = f"{num1} {op} {num2}"
         index += 1
+        print("Question:", answer)
         user_answer = prompt.string("Your answer: ")
-        answer = op(beg1, beg2)
-        print ("Question:'{}{}{}?".format(beg1, op, beg2))
-        
-
-
+        if "+" == op:
+            answer = str(num1 + num2)
+        if "-" == op:
+            answer = str(num1 - num2)
+        if "*" == op:
+            answer = str(num1 * num2)
+        if user_answer == answer:
+            print("Correct!")
+            if index == winscore:
+                print(congrats)
+        else:
+            print(f"{user_answer} is wrong answer ;(. Correct answer was,", answer)
+            break
