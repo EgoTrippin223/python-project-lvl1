67afe23353dad2278daae6b6935de1dabb259c6f
Даниил Филимошин <filimoshin03@inbox.ru>
2022-09-08 11:10:32 +1000

    delete some files

--- brain_games/scripts/games/logic_calc.py
+++
@@ -1,9 +0,0 @@
-import prompt
-
-
-def greeting():
-    print("Welcome to the Brain Games!")
-    user_name = prompt.string("May I have your name? ")
-    print(f"Hello, {user_name}!")
-    print("What is the result of the expression?")
-    congrats = f"Congratulations, {user_name}"
---
+++ brain_games/scripts/logic.py
@@ -0,0 +1,31 @@
+import prompt
+
+
+def greeting():
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string("May I have your name? ")
+    print(f"Hello, {user_name}!")
+
+
+def even(number):
+    if number % 2 == 0:
+        return "yes"
+    else:
+        return "no"
+
+
+
+def is_prime(number):
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return "no"
+    return "yes"
+
+
+def loop_function(function):
+    for i in range(3):
+        function()
+
+def user_question(quest):
+    print("Question:", quest)
+    user_answer = prompt.string("Your answer: ")
