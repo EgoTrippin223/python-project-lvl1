a2ad9ee2c3b03afc3d95fc6504d5ed088d1dddef
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-13 08:20:27 +1000

    added missed dot

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,7 +2,7 @@ import random
 from brain_games.brain_engine import MAX_VALUE
 
 
-BRAIN_TASK = "Answer 'yes' if the number is even, otherwise answer 'no'"
+BRAIN_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def get_question_and_answer():
     rand = random.randint(0, MAX_VALUE)
