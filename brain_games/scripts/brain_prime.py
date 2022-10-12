ddf764ab37854fac35bf00bf0927d97200d794c5
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-13 08:10:56 +1000

    fix brain_prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 from brain_games.brain_engine import brain_engine
-from brain_games.games.prime import get_question_and_answer
 from brain_games.games.prime import BRAIN_TASK
+from brain_games.games.prime import get_question_and_answer
 
 
 
 def main():
-    brain_engine(get_question_and_answer, BRAIN_TASK)
+    brain_engine(BRAIN_TASK, get_question_and_answer)
 
 
 if __name__ == "__main__":
