a88b75dfc9385f198292ba984a8a235e979dc81f
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-13 08:59:44 +1000

    fix brain_calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,12 +1,12 @@
-import random, operator
+import random
 
 
 BRAIN_TASK = "What is the result of the expression?"
 
 
 def get_question_and_answer():
-    operator = ["-", "+", "*"]
-    op = random.choice(operator)
+    operation = ["-", "+", "*"]
+    op = random.choice(operation)
     num1 = random.randint(10, 30)
     num2 = random.randint(1, 10)
     question = f"{num1} {op} {num2}"
