b6aac5f2215099a55cbf84a81c19af20ea62e7d5
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-17 11:30:32 +1000

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,18 +1,27 @@
 import random
+from brain_games.brain_engine import MAX_VALUE
 
-
+MIN_LENGTH = 5
+MAX_LENGT = 10
+STEP = 10
 BRAIN_TASK = "What number is missing in the progression ?"
 
 
 def get_question_and_answer():
-    numbers = []
-    num1 = random.randint(1, 5)
-    num2 = random.randint(30, 45)
-    n = random.randint(2, 4)
-    for i in range(num1, num2, n):
-        numbers.append(i)
-    correct_answer = random.choice(numbers)
-    index_x = numbers.index(correct_answer)
-    numbers[index_x] = ".."
-    question = " ".join(map(str, numbers))
-    return question, correct_answer
+    array = get_sequence()
+    hidden_index = random.randint(0, len(array) - 1)
+    question = (
+        f"{' '.join(array[:hidden_index])} .. "
+        f"{' '.join(array[hidden_index + 1:])}".strip()
+    )
+    answer = array[hidden_index]
+    question_and_answer = (question, answer)
+    return question_and_answer
+
+
+def get_sequence():
+    initial_term = random.randint(0, MAX_VALUE)
+    number_of_terms = random.randint(MIN_LENGTH, MAX_LENGT)
+    step = random.randint(1, STEP)
+    last_term = initial_term + step * number_of_terms
+    return [str(x) for x in range(initial_term, last_term, step)]
