0513c5f25573324f47c20d62c273e52ab4936e10
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-17 11:07:55 +1000

    formatted PEP8 prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,9 +9,9 @@ def get_question_and_answer():
     question_and_answer = (num, is_prime(num) and "yes" or "no")
     return question_and_answer
 
+
 def is_prime(number):
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
     return True
-
