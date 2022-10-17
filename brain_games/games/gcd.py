88866dfa8bb2659c99e864773ac9fcc3fcdaa5ce
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-17 11:16:27 +1000

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,6 +8,10 @@ BRAIN_TASK = "Find the greatest common divisor of given numbers."
 def get_question_and_answer():
     num1 = randint(21, 99)
     num2 = randint(25, 63)
-    question = f"{num1} {num2}"
-    correct_answer = math.gcd(num1, num2)
-    return question, correct_answer
+    question_and_answer = (f"{num1} {num2}", str(gcd(num1, num2)))
+    correct_answer = gcd(num1, num2)
+    return question_and_answer
+
+
+def gcd(number1, number2):
+    return math.gcd(number1, number2)
