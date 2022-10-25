9dbfe71bafcffc768598c915182e93748426f98e
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-25 12:32:50 +1000

    add constants

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,13 +2,13 @@ import random
 
 
 RULES = "What is the result of the expression?"
-
+FIRST_NUM = random.randint(10, 30)
+SECOND_NUM = random.randint(1, 10)
 
 def get_question_and_answer():
     operation = ["-", "+", "*"]
     op = random.choice(operation)
-    FIRST_NUM = random.randint(10, 30)
-    SECOND_NUM = random.randint(1, 10)
+    
     question = f"{FIRST_NUM} {op} {SECOND_NUM}"
     if "+" == op:
         correct_answer = str(FIRST_NUM + SECOND_NUM)
