1648c050b38b4afd0884c6746abbd4137a630917
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-25 13:42:35 +1000

    redact scripts

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
-from brain_games.brain_engine import start_game
+from brain_games import brain_engine
 from brain_games.games.prime import RULES
 from brain_games.games.prime import get_question_and_answer
 
 
 def main():
-    start_game(RULES, get_question_and_answer)
+    brain_engine.start_game(RULES, get_question_and_answer)
 
 
 if __name__ == "__main__":
