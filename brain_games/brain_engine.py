13dfacaa70c35e0eda78e010fe34772519cf50b3
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-25 20:41:00 +1000

    redact brain_engine

--- brain_games/brain_engine.py
+++ brain_games/brain_engine.py
@@ -4,15 +4,15 @@ ITERATIONS_NUMBER = 3  # кол-во вопросов игры
 MAX_VALUE = 100
 
 
-def start_game(RULES, question_and_answer):
+def start_game(game):
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
-    print(RULES)
+    print(game.RULES)
     index = 0
     while index < ITERATIONS_NUMBER:
         index += 1
-        (question, correct_answer) = question_and_answer()
+        (question, correct_answer) = game.get_question_and_answer()
         print("Question:", question)
         user_answer = prompt.string("Your answer: ")
         if user_answer == str(correct_answer):
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,9 @@
-from brain_games.games.calc import RULES
-from brain_games.games.calc import get_question_and_answer
+from brain_games.games import calc
 from brain_games import brain_engine
 
 
 def main():
-    brain_engine.start_game(RULES, get_question_and_answer)
+  brain_engine.start_game(calc)
 
 
 if __name__ == "__main__":
