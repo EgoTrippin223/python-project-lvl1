8ac7e7e4f279e950691307e19e2dc730b0b34bca
Даниил Филимошин <filimoshin03@inbox.ru>
2022-10-26 11:33:07 +1000

    redact progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,16 +4,19 @@ import random
 RULES = "What number is missing in the progression ?"
 
 
-def get_question_and_answer():
-    numbers = []
+def prog():
     FIRST_NUM = random.randint(1, 5)
-    SECOND_NUM = random.randint(30, 45)
-    STEP = random.randint(2, 4)
-    for i in range(FIRST_NUM, SECOND_NUM, STEP):
-        numbers.append(i)
-    correct_answer = random.choice(numbers)
-    index_x = numbers.index(correct_answer)
-    numbers[index_x] = ".."
-    question = " ".join(map(str, numbers))
+    SECOND_NUM = random.randint(30, 35)
+    STEP = random.randint(2, 3)
+    progression = list(range(FIRST_NUM, SECOND_NUM, STEP))
+    return progression
+
+
+def get_question_and_answer():
+    progression = prog()
+    correct_answer = random.choice(progression)
+    index_x = progression.index(correct_answer)
+    progression[index_x] = ".."
+    question = " ".join(map(str, progression))
     question_and_answer = (question, correct_answer)
     return question_and_answer
