8656296c3feba0388ebb41dce4defa7c992b3abe
Даниил Филимошин <filimoshin03@inbox.ru>
2022-11-03 07:55:40 +1000

    some fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,14 +1,16 @@
 import random
+from random import randint
 
 
 RULES = "What is the result of the expression?"
-
+MAX_VALUE_FIRST_NUM = 20
+MAX_VALUE_SECOND_NUM = 10
 
 def get_question_and_answer():
     operation = ["-", "+", "*"]
     op = random.choice(operation)
-    FIRST_NUM = random.randint(10, 30)
-    SECOND_NUM = random.randint(1, 10)
+    FIRST_NUM = randint(0, MAX_VALUE_FIRST_NUM)
+    SECOND_NUM = randint(1, MAX_VALUE_SECOND_NUM)
     question = f"{FIRST_NUM} {op} {SECOND_NUM}"
     if "+" == op:
         correct_answer = str(FIRST_NUM + SECOND_NUM)
