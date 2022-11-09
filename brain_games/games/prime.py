189cf433b499cf4ebaf2f89511a396427de14598
Даниил Филимошин <filimoshin03@inbox.ru>
2022-11-09 10:52:26 +1000

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,13 +5,13 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question_and_answer():
-    NUM = random.randint(2, MAX_VALUE)
-    question_and_answer = (NUM, answer(NUM))
+    num = random.randint(2, MAX_VALUE)
+    question_and_answer = (num, get_answer(num))
     return question_and_answer
 
 
-def answer(number):
-    if is_prime(number) is True:
+def get_answer(number):
+    if is_prime(number):
         return "yes"
     else:
         return "no"
